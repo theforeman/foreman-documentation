b3b31439d70ac7cb07c91af2845a76ac2b970673
Lukas Zapletal <lzap+git@redhat.com>
2020-02-21 11:45:25 +0100

    Removed publish.sh script

--- publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pushd guides
-make clean html
-rsync -vr build/ web02.theforeman.org:/var/www/vhosts/docs/htdocs/current/
-popd
-rsync -vr web/ web02.theforeman.org:/var/www/vhosts/docs/htdocs/
