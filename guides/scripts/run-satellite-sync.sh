42c67ac543ee9562fa0d846d53c10b79bb3c12da
Sergei Petrosian <spetrosi@redhat.com>
2020-09-10 09:05:08 +0200

    Add description

--- guides/scripts/run-satellite-sync.sh
+++ guides/scripts/run-satellite-sync.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# ##################################################
+# Created on: Thu, 10 Sep 2020
+# Created by: Sergei Petrosian
+# Run this script from the directory where the satellite-sync.sh script is stored to run satellite-sync.sh for each downstream branch.
+# ##################################################
+
 for BRANCH in master $(git branch --list 'SATELLITE-*')
 do
   if [ $BRANCH == "master" ]
