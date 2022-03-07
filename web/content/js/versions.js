theforeman/foreman-documentation

commit 94bc6cdf7ae08ae87605ab723497ac02ba963c7b

Differentiate guides by version
When a guide is added, it is not always included in all older versions.
This changes the navigation to be version aware. This allows only
showing certain builds per version and certain guides per build.

For example, prior to Foreman 3.1 the Foreman on EL/Deb guides were
unreviewed. This chooses not to show them, but does show the Katello
guides since those were considered production starting Foreman 2.4.

Another example is the Release Notes guide which was introduced in
nightly and then picked to all 3.x versions. This change allows not
presenting the link in the 2.x guides.

This also removes all non-Katello links from non-nightly landing pages.
This makes it clear that only Katello documentation should be considered
ready.

A future step is to collect the per version builds dynamically based on
which branches were built. This means the version is really maintained
inside that branch rather than everything inside master. That makes it
possible to cherry pick a new guide including the link. Whenever the
cherry pick is merged, the navigation is correctly updated. However,
that is not part of this.

web/content/js/versions.js
@@ -1,0 +1,526 @@
+const navVersions = [
+  {
+    title: "Nightly",
+    path: "nightly",
+    builds: [
+      {
+        title: "Foreman on EL",
+        filename: "index-foreman-el.html",
+        guides: [
+          {
+            title: "Release Notes",
+            path: "Release_notes"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Foreman Server",
+            path: "Installing_Server"
+          },
+          {
+            title: "Installing Smart Proxy",
+            path: "Installing_Proxy"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Managing Hosts Guide",
+            path: "Managing_Hosts"
+          },
+          {
+            title: "Configuring Hosts Using Puppet",
+            path: "Managing_Configurations_Puppet"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          },
+          {
+            title: "Application Centric Deployment",
+            path: "Application_Centric_Deployment"
+          }
+        ]
+      },
+      {
+        title: "Foreman on Debian",
+        filename: "index-foreman-deb.html",
+        guides: [
+          {
+            title: "Release Notes",
+            path: "Release_notes"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Foreman Server",
+            path: "Installing_Server"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Configuring Hosts Using Puppet",
+            path: "Managing_Configurations_Puppet"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          }
+        ]
+      },
+      {
+        title: "Katello on EL",
+        filename: "index-katello.html",
+        guides: [
+          {
+            title: "Release Notes",
+            path: "Release_notes"
+          },
+          {
+            title: "Planning Guide",
+            path: "Planning_Guide"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Katello Server",
+            path: "Installing_Server"
+          },
+          {
+            title: "Installing Smart Proxy with Content",
+            path: "Installing_Proxy"
+          },
+          {
+            title: "Upgrading and Updating",
+            path: "Upgrading_and_Updating"
+          },
+          {
+            title: "Configuring Smart Proxies with a Load Balancer",
+            path: "Configuring_Load_Balancer"
+          },
+          {
+            title: "Content Management Guide",
+            path: "Content_Management_Guide"
+          },
+          {
+            title: "Converting a Host to RHEL",
+            path: "Converting_a_Host_to_RHEL"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Managing Hosts Guide",
+            path: "Managing_Hosts"
+          },
+          {
+            title: "Configuring Hosts Using Puppet",
+            path: "Managing_Configurations_Puppet"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          },
+          {
+            title: "Application Centric Deployment",
+            path: "Application_Centric_Deployment"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: "Foreman 3.2 - Katello 4.4 (stable)",
+    path: "3.2",
+    builds: [
+      {
+        title: "Katello on EL",
+        filename: "index-katello.html",
+        guides: [
+          {
+            title: "Release Notes",
+            path: "Release_notes"
+          },
+          {
+            title: "Planning Guide",
+            path: "Planning_Guide"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Katello Server",
+            path: "Installing_Server"
+          },
+          {
+            title: "Installing Smart Proxy with Content",
+            path: "Installing_Proxy"
+          },
+          {
+            title: "Upgrading and Updating",
+            path: "Upgrading_and_Updating"
+          },
+          {
+            title: "Configuring Smart Proxies with a Load Balancer",
+            path: "Configuring_Load_Balancer"
+          },
+          {
+            title: "Content Management Guide",
+            path: "Content_Management_Guide"
+          },
+          {
+            title: "Converting a Host to RHEL",
+            path: "Converting_a_Host_to_RHEL"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Managing Hosts Guide",
+            path: "Managing_Hosts"
+          },
+          {
+            title: "Configuring Hosts Using Puppet",
+            path: "Managing_Configurations_Puppet"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          },
+          {
+            title: "Application Centric Deployment",
+            path: "Application_Centric_Deployment"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: "Foreman 3.1 - Katello 4.3 (stable)",
+    path: "3.1",
+    builds: [
+      {
+        title: "Katello on EL",
+        filename: "index-foreman-deb.html",
+        guides: [
+          {
+            title: "Release Notes",
+            path: "Release_notes"
+          },
+          {
+            title: "Planning Guide",
+            path: "Planning_Guide"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Katello Server",
+            path: "Installing_Server"
+          },
+          {
+            title: "Installing Smart Proxy with Content",
+            path: "Installing_Proxy"
+          },
+          {
+            title: "Upgrading and Updating",
+            path: "Upgrading_and_Updating"
+          },
+          {
+            title: "Configuring Smart Proxies with a Load Balancer",
+            path: "Configuring_Load_Balancer"
+          },
+          {
+            title: "Content Management Guide",
+            path: "Content_Management_Guide"
+          },
+          {
+            title: "Converting a Host to RHEL",
+            path: "Converting_a_Host_to_RHEL"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Managing Hosts Guide",
+            path: "Managing_Hosts"
+          },
+          {
+            title: "Configuring Hosts Using Puppet",
+            path: "Managing_Configurations_Puppet"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          },
+          {
+            title: "Application Centric Deployment",
+            path: "Application_Centric_Deployment"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: "Foreman 3.0 - Katello 4.2 (supported)",
+    path: "3.0",
+    builds: [
+      {
+        title: "Katello on EL",
+        filename: "index-foreman-deb.html",
+        guides: [
+          {
+            title: "Release Notes",
+            path: "Release_notes"
+          },
+          {
+            title: "Planning Guide",
+            path: "Planning_Guide"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Katello Server",
+            path: "Installing_Server_on_Red_Hat"
+          },
+          {
+            title: "Installing Smart Proxy with Content",
+            path: "Installing_Proxy_on_Red_Hat"
+          },
+          {
+            title: "Upgrading and Updating",
+            path: "Upgrading_and_Updating"
+          },
+          {
+            title: "Configuring Smart Proxies with a Load Balancer",
+            path: "Configuring_Load_Balancer"
+          },
+          {
+            title: "Content Management Guide",
+            path: "Content_Management_Guide"
+          },
+          {
+            title: "Converting a Host to RHEL",
+            path: "Converting_a_Host_to_RHEL"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Managing Hosts Guide",
+            path: "Managing_Hosts"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          },
+          {
+            title: "Application Centric Deployment",
+            path: "Application_Centric_Deployment"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: "Foreman 2.5 - Katello 4.1 (unsupported)",
+    path: "2.5",
+    builds: [
+      {
+        title: "Katello on EL",
+        filename: "index-foreman-deb.html",
+        guides: [
+          {
+            title: "Planning Guide",
+            path: "Planning_Guide"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Katello Server",
+            path: "Installing_Server_on_Red_Hat"
+          },
+          {
+            title: "Installing Smart Proxy with Content",
+            path: "Installing_Proxy_on_Red_Hat"
+          },
+          {
+            title: "Upgrading and Updating",
+            path: "Upgrading_and_Updating"
+          },
+          {
+            title: "Configuring Smart Proxies with a Load Balancer",
+            path: "Configuring_Load_Balancer"
+          },
+          {
+            title: "Content Management Guide",
+            path: "Content_Management_Guide"
+          },
+          {
+            title: "Converting a Host to RHEL",
+            path: "Converting_a_Host_to_RHEL"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Managing Hosts Guide",
+            path: "Managing_Hosts"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          },
+          {
+            title: "Application Centric Deployment",
+            path: "Application_Centric_Deployment"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: "Foreman 2.4 - Katello 4.0 (unsupported)",
+    path: "2.4",
+    builds: [
+      {
+        title: "Katello on EL",
+        filename: "index-foreman-deb.html",
+        guides: [
+          {
+            title: "Planning Guide",
+            path: "Planning_Guide"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Katello Server",
+            path: "Installing_Server_on_Red_Hat"
+          },
+          {
+            title: "Installing Smart Proxy with Content",
+            path: "Installing_Proxy_on_Red_Hat"
+          },
+          {
+            title: "Upgrading and Updating",
+            path: "Upgrading_and_Updating"
+          },
+          {
+            title: "Configuring Smart Proxies with a Load Balancer",
+            path: "Configuring_Load_Balancer"
+          },
+          {
+            title: "Content Management Guide",
+            path: "Content_Management_Guide"
+          },
+          {
+            title: "Converting a Host to RHEL",
+            path: "Converting_a_Host_to_RHEL"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Managing Hosts Guide",
+            path: "Managing_Hosts"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          },
+          {
+            title: "Application Centric Deployment",
+            path: "Application_Centric_Deployment"
+          }
+        ]
+      }
+    ]
+  }
+]

guides/common/linkchecker.ini
@@ -7,6 +7,7 @@ maxrunseconds=900
 ignore=example.com
   cdn.redhat.com
   file:///modules
+  file:///js/versions.js
   file:///js/nav.js
   tools.ietf.org
   host/unattended/provision

web/content/js/nav.js
@@ -1,142 +1,89 @@
-let navVersions = [
-{
-	title: "Version", items: [
-	{
-		title: "Nightly",
-		href: "nightly"
-	},{
-		title: "Foreman 3.2 - Katello 4.4 (RC-phase)",
-		href: "3.2"
-	},{
-		title: "Foreman 3.1 - Katello 4.3 (stable)",
-		href: "3.1"
-	},{
-		title: "Foreman 3.0 - Katello 4.2 (supported)",
-		href: "3.0"
-	},{
-		title: "Foreman 2.5 - Katello 4.1 (unsupported)",
-		href: "2.5"
-	},{
-		title: "Foreman 2.4 - Katello 4.0 (unsupported)",
-		href: "2.4"
-	}]
-    }
-]
+function buildNavigation() {
+  const versionRe = new RegExp(`/(nightly|\\d+.\\d+)/`);
+  var currentVer = document.location.toString().match(versionRe);
+  if (currentVer && currentVer.length > 1) {
+    currentVer = currentVer[1];
+  } else {
+    currentVer = "nightly";
+  }
+  const navBuilds = navVersions.find(function(version) {
+    return version['path'] == currentVer;
+  }).builds;
 
-let navItems = [
-{
-	title: "Foreman on EL", items: [
-	{
-		title: "Release Notes",
-		href: "/Release_notes/index-foreman-el.html"
-	},{
-		title: "Quickstart Guide",
-		href: "/Quickstart_Guide/index-foreman-el.html"
-	},{
-		title: "Installing Foreman Server",
-		href: "/Installing_Server/index-foreman-el.html"
-	},{
-		title: "Installing Smart Proxy",
-		href: "/Installing_Proxy/index-foreman-el.html"
-	},{
-		title: "Deploying Foreman on AWS",
-		href: "/Deploying_on_AWS/index-foreman-el.html"
-	},{
-		title: "Provisioning Guide",
-		href: "/Provisioning_Guide/index-foreman-el.html"
-	},{
-		title: "Configuring Foreman to use Ansible",
-		href: "/Configuring_Ansible/index-foreman-el.html"
-	},{
-		title: "Managing Hosts Guide",
-		href: "/Managing_Hosts/index-foreman-el.html"
-	},{
-		title: "Configuring Hosts Using Puppet",
-		href: "/Managing_Configurations_Puppet/index-foreman-el.html"
-	},{
-		title: "Administering Foreman",
-		href: "/Administering_Red_Hat_Satellite/index-foreman-el.html"
-	},{
-		title: "Application Centric Deployment",
-		href: "/Application_Centric_Deployment/index-foreman-el.html"
-	}]
-},{
-	title: "Foreman on Debian", items: [
-	{
-		title: "Release Notes",
-		href: "/Release_notes/index-foreman-deb.html"
-	},{
-		title: "Quickstart Guide",
-		href: "/Quickstart_Guide/index-foreman-deb.html"
-	},{
-		title: "Installing Foreman Server",
-		href: "/Installing_Server/index-foreman-deb.html"
-	},{
-		title: "Deploying Foreman on AWS",
-		href: "/Deploying_on_AWS/index-foreman-deb.html"
-	},{
-		title: "Provisioning Guide",
-		href: "/Provisioning_Guide/index-foreman-deb.html"
-	},{
-		title: "Configuring Foreman to use Ansible",
-		href: "/Configuring_Ansible/index-foreman-deb.html"
-	},{
-		title: "Configuring Hosts Using Puppet",
-		href: "/Managing_Configurations_Puppet/index-foreman-deb.html"
-	},{
-		title: "Administering Foreman",
-		href: "/Administering_Red_Hat_Satellite/index-foreman-deb.html"
-	}]
-},{
-	title: "Katello on EL", items: [
-	{
-		title: "Release Notes",
-		href: "/Release_notes/index-katello.html"
-	},{
-		title: "Planning Guide",
-		href: "/Planning_Guide/index-katello.html"
-	},{
-		title: "Quickstart Guide",
-		href: "/Quickstart_Guide/index-katello.html"
-	},{
-		title: "Installing Katello Server",
-		href: "/Installing_Server/index-katello.html"
-	},{
-		title: "Installing Smart Proxy with Content",
-		href: "/Installing_Proxy/index-katello.html"
-	},{
-		title: "Upgrading and Updating",
-		href: "/Upgrading_and_Updating/index-katello.html"
-	},{
-		title: "Configuring Smart Proxies with a Load Balancer",
-		href: "/Configuring_Load_Balancer/index-katello.html"
-	},{
-		title: "Content Management Guide",
-		href: "/Content_Management_Guide/index-katello.html"
-	},{
-		title: "Converting a Host to RHEL",
-		href: "/Converting_a_Host_to_RHEL/index-katello.html"
-	},{
-		title: "Deploying Foreman on AWS",
-		href: "/Deploying_on_AWS/index-katello.html"
-	},{
-		title: "Provisioning Guide",
-		href: "/Provisioning_Guide/index-katello.html"
-	},{
-		title: "Configuring Foreman to use Ansible",
-		href: "/Configuring_Ansible/index-katello.html"
-	},{
-		title: "Managing Hosts Guide",
-		href: "/Managing_Hosts/index-katello.html"
-	},{
-		title: "Configuring Hosts Using Puppet",
-		href: "/Managing_Configurations_Puppet/index-katello.html"
-	},{
-		title: "Administering Foreman",
-		href: "/Administering_Red_Hat_Satellite/index-katello.html"
-	},{
-		title: "Application Centric Deployment",
-		href: "/Application_Centric_Deployment/index-katello.html"
-	}]
+  return `<nav>
+  <a href="/">
+    <img class="logo" src="/img/helmet.svg" alt="Home">
+  </a>
+<button type="button" class="btn-hamburger" data-action="nav-toggle">
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+</button>
+<ul class="nav-menu">
+  <li class="nav-item"><a href="/">Home</a></li>`
+    + navBuilds.map(function(build){
+      return(
+        `<li class="nav-item dropdown">
+          <a href="#" data-action="dropdown-toggle">${build.title}</a>
+            <div class="dropdown-menu">`
+            + build.guides.map(function(guide){
+              const url = `/${currentVer}/${guide.path}/${build.filename}`;
+              return `<div class="dropdown-div"><a class="dropdown-item" href="${url}">${guide.title}</a></div>`;
+            }).join("")
+            +`</div>
+        </li>`
+      )}).join("")
+    + `<li class="nav-item dropdown">
+        <a href="#" data-action="dropdown-toggle">Version ${currentVer}</a>
+        <div class="dropdown-menu dropdown-menu-left">`
+    + navVersions.map(function(version) {
+      if (document.location.pathname == "/") {
+        var dl = `/release/${version.path}`;
+      } else {
+        var dl = document.location.toString().replace(versionRe, `/${version.path}/`);
+      }
+      return(`<div class="dropdown-div"><a class="dropdown-item" href=${dl}>${version.title}</a></div>`
+      )}).join("")
+    +`</div>
+    </li>
+  </ul>
+</nav>`;
+}
+
+function toggleElement(dropdown) {
+  if (dropdown.classList.contains("show")) {
+    dropdown.classList.remove("show");
+  } else {
+    dropdown.classList.add("show");
+  }
+}
+
+function closeAll(except) {
+  const elems = document.getElementsByClassName("nav-item");
+  for (let i = 0; i < elems.length; i++) {
+    const elem = elems[i];
+    if (elem != except) {
+      elem.classList.remove("show");
     }
-]
+  };
+}
+
+var addNavbarListeners = () => {
+  const nav = document.querySelector("nav");
+  nav.querySelectorAll("[data-action='dropdown-toggle']").forEach((dropdownToggle) => {
+    dropdownToggle.addEventListener("click", () => {
+      closeAll(dropdownToggle.parentElement);
+      toggleElement(dropdownToggle.parentElement);
+    });
+  });
+
+  nav.querySelector("[data-action='nav-toggle']").addEventListener("click", () => {
+    if (nav.classList.contains("opened")) {
+      nav.classList.remove("opened");
+    } else {
+      nav.classList.add("opened");
+    }
+  });
+}

web/layouts/default.html.erb
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <title>Foreman documentation: <%= @item[:title] %></title>
     <link rel="stylesheet" href="/css/default.css">
-    <script src="/js/nav.js"></script>
     <meta name="generator" content="Nanoc <%= Nanoc::VERSION %>">
   </head>
   <body>

web/layouts/nav.html.erb
@@ -1,95 +1,10 @@
-<!--
-	vim: set noai ts=2 sts=2 sw=2 et ft= syn=html
--->
-<div id="wrap">
-<script src="/js/nav.js"></script>
-<script>
-const versionRe = new RegExp(`/(nightly|\\d+.\\d+)/`);
-var currentVer = document.location.toString().match(versionRe);
-if (currentVer && currentVer.length > 1) {
-  currentVer = currentVer[1];
-} else {
-  currentVer = "nightly";
-}
-var navId = 1;
-var Navbar = `<nav>
-<img class="logo" src="/img/helmet.svg" />
-<button type="button" class="btn-hamburger" data-action="nav-toggle">
-  <span></span>
-  <span></span>
-  <span></span>
-  <span></span>
-  <span></span>
-</button>
-<ul class="nav-menu">
-  <li class="nav-item"><a href="/">Home</a></li>`
-  + navItems.map(function(navItem){ return(
-    `<li id="nav-id-`+ navId++ +`" class="nav-item dropdown">
-        <a href="#" data-action="dropdown-toggle">`+ navItem.title + `</a>
-        <div class="dropdown-menu">`
-        + navItem.items.map(function(item){ return(
-            `<div class="dropdown-div"><a class="dropdown-item" href=/`+currentVer+item.href+`>`+item.title+`</a></div>`
-        )}).join("")
-        +`</div>
-    </li>`
-    )}).join("")
-  + navVersions.map(function(navItem){ return(
-    `<li id="nav-id-`+ navId++ +`" class="nav-item dropdown">
-        <a href="#" data-action="dropdown-toggle">Version `+currentVer+`</a>
-        <div class="dropdown-menu dropdown-menu-left">`
-        + navItem.items.map(function(item){
-            if (document.location.pathname == "/") {
-                var dl = "/release/" + item.href;
-            } else {
-                var dl = document.location.toString().replace(versionRe, "/"+item.href+"/");
-            }
-            return(`<div class="dropdown-div"><a class="dropdown-item" href=`+dl+`>`+item.title+`</a></div>`
-        )}).join("")
-        +`</div>
-    </li>`
-    )}).join("")
-+`</ul>
-</nav>`;
-
-function toggleElement(dropdown) {
-  if (dropdown.classList.contains("show")) {
-    dropdown.classList.remove("show");
-  } else {
-    dropdown.classList.add("show");
-  }
-}
-
-function closeAll(except) {
-  for (let i = 1; i <= navItems.length + 1; i++) {
-    let elem = document.getElementById("nav-id-" + i)
-    if (elem != except) {
-      elem.classList.remove("show");
-    }
-  }
-}
-
-var addNavbarListeners = () => {
-  let nav = document.querySelector("nav");
-  let navId = 1;
-  nav.querySelectorAll("[data-action='dropdown-toggle']").forEach((dropdownToggle) => {
-    let dropdown = document.getElementById("nav-id-" + navId++);
-    dropdownToggle.addEventListener("click", () => {
-      closeAll(dropdown);
-      toggleElement(dropdown);
-    });
-  });
-
-  nav.querySelector("[data-action='nav-toggle']").addEventListener("click", () => {
-    if (nav.classList.contains("opened")) {
-      nav.classList.remove("opened");
-    } else {
-      nav.classList.add("opened");
-    }
-  });
-}
-document.open();
-document.write(Navbar)
-addNavbarListeners();
-document.close();
-</script>
-</div>
+    <div id="wrap">
+      <script src="/js/versions.js"></script>
+      <script src="/js/nav.js"></script>
+      <script>
+        document.open();
+        document.write(buildNavigation());
+        addNavbarListeners();
+        document.close();
+      </script>
+    </div>

commit 38a7698b2ae67ffb7d1a7454ad6a8ac07634af3f

Correct the Foreman 3.2 state in navigation
Fixes: 94bc6cdf7ae08ae87605ab723497ac02ba963c7b

web/content/js/versions.js
@@ -164,7 +164,7 @@ const navVersions = [
     ]
   },
   {
-    title: "Foreman 3.2 - Katello 4.4 (stable)",
+    title: "Foreman 3.2 - Katello 4.4 (RC-phase)",
     path: "3.2",
     builds: [
       {