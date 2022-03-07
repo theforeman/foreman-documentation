982b00e869827bdca70ef22294068950269e6eab
Lukas Zapletal <lzap+git@redhat.com>
2022-03-07 17:22:21 +0100

    Differentiate guides by version

---
+++ web/content/js/versions.js
@@ -0,0 +1,526 @@
+const navVersions = [
+  {
+    title: "Nightly",
+    path: "nightly",
+    builds: [
+      {
+        title: "Foreman on EL",
+        filename: "index-foreman-el.html",
+        guides: [
+          {
+            title: "Release Notes",
+            path: "Release_notes"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Foreman Server",
+            path: "Installing_Server"
+          },
+          {
+            title: "Installing Smart Proxy",
+            path: "Installing_Proxy"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Managing Hosts Guide",
+            path: "Managing_Hosts"
+          },
+          {
+            title: "Configuring Hosts Using Puppet",
+            path: "Managing_Configurations_Puppet"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          },
+          {
+            title: "Application Centric Deployment",
+            path: "Application_Centric_Deployment"
+          }
+        ]
+      },
+      {
+        title: "Foreman on Debian",
+        filename: "index-foreman-deb.html",
+        guides: [
+          {
+            title: "Release Notes",
+            path: "Release_notes"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Foreman Server",
+            path: "Installing_Server"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Configuring Hosts Using Puppet",
+            path: "Managing_Configurations_Puppet"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          }
+        ]
+      },
+      {
+        title: "Katello on EL",
+        filename: "index-katello.html",
+        guides: [
+          {
+            title: "Release Notes",
+            path: "Release_notes"
+          },
+          {
+            title: "Planning Guide",
+            path: "Planning_Guide"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Katello Server",
+            path: "Installing_Server"
+          },
+          {
+            title: "Installing Smart Proxy with Content",
+            path: "Installing_Proxy"
+          },
+          {
+            title: "Upgrading and Updating",
+            path: "Upgrading_and_Updating"
+          },
+          {
+            title: "Configuring Smart Proxies with a Load Balancer",
+            path: "Configuring_Load_Balancer"
+          },
+          {
+            title: "Content Management Guide",
+            path: "Content_Management_Guide"
+          },
+          {
+            title: "Converting a Host to RHEL",
+            path: "Converting_a_Host_to_RHEL"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Managing Hosts Guide",
+            path: "Managing_Hosts"
+          },
+          {
+            title: "Configuring Hosts Using Puppet",
+            path: "Managing_Configurations_Puppet"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          },
+          {
+            title: "Application Centric Deployment",
+            path: "Application_Centric_Deployment"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: "Foreman 3.2 - Katello 4.4 (stable)",
+    path: "3.2",
+    builds: [
+      {
+        title: "Katello on EL",
+        filename: "index-katello.html",
+        guides: [
+          {
+            title: "Release Notes",
+            path: "Release_notes"
+          },
+          {
+            title: "Planning Guide",
+            path: "Planning_Guide"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Katello Server",
+            path: "Installing_Server"
+          },
+          {
+            title: "Installing Smart Proxy with Content",
+            path: "Installing_Proxy"
+          },
+          {
+            title: "Upgrading and Updating",
+            path: "Upgrading_and_Updating"
+          },
+          {
+            title: "Configuring Smart Proxies with a Load Balancer",
+            path: "Configuring_Load_Balancer"
+          },
+          {
+            title: "Content Management Guide",
+            path: "Content_Management_Guide"
+          },
+          {
+            title: "Converting a Host to RHEL",
+            path: "Converting_a_Host_to_RHEL"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Managing Hosts Guide",
+            path: "Managing_Hosts"
+          },
+          {
+            title: "Configuring Hosts Using Puppet",
+            path: "Managing_Configurations_Puppet"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          },
+          {
+            title: "Application Centric Deployment",
+            path: "Application_Centric_Deployment"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: "Foreman 3.1 - Katello 4.3 (stable)",
+    path: "3.1",
+    builds: [
+      {
+        title: "Katello on EL",
+        filename: "index-foreman-deb.html",
+        guides: [
+          {
+            title: "Release Notes",
+            path: "Release_notes"
+          },
+          {
+            title: "Planning Guide",
+            path: "Planning_Guide"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Katello Server",
+            path: "Installing_Server"
+          },
+          {
+            title: "Installing Smart Proxy with Content",
+            path: "Installing_Proxy"
+          },
+          {
+            title: "Upgrading and Updating",
+            path: "Upgrading_and_Updating"
+          },
+          {
+            title: "Configuring Smart Proxies with a Load Balancer",
+            path: "Configuring_Load_Balancer"
+          },
+          {
+            title: "Content Management Guide",
+            path: "Content_Management_Guide"
+          },
+          {
+            title: "Converting a Host to RHEL",
+            path: "Converting_a_Host_to_RHEL"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Managing Hosts Guide",
+            path: "Managing_Hosts"
+          },
+          {
+            title: "Configuring Hosts Using Puppet",
+            path: "Managing_Configurations_Puppet"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          },
+          {
+            title: "Application Centric Deployment",
+            path: "Application_Centric_Deployment"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: "Foreman 3.0 - Katello 4.2 (supported)",
+    path: "3.0",
+    builds: [
+      {
+        title: "Katello on EL",
+        filename: "index-foreman-deb.html",
+        guides: [
+          {
+            title: "Release Notes",
+            path: "Release_notes"
+          },
+          {
+            title: "Planning Guide",
+            path: "Planning_Guide"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Katello Server",
+            path: "Installing_Server_on_Red_Hat"
+          },
+          {
+            title: "Installing Smart Proxy with Content",
+            path: "Installing_Proxy_on_Red_Hat"
+          },
+          {
+            title: "Upgrading and Updating",
+            path: "Upgrading_and_Updating"
+          },
+          {
+            title: "Configuring Smart Proxies with a Load Balancer",
+            path: "Configuring_Load_Balancer"
+          },
+          {
+            title: "Content Management Guide",
+            path: "Content_Management_Guide"
+          },
+          {
+            title: "Converting a Host to RHEL",
+            path: "Converting_a_Host_to_RHEL"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Managing Hosts Guide",
+            path: "Managing_Hosts"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          },
+          {
+            title: "Application Centric Deployment",
+            path: "Application_Centric_Deployment"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: "Foreman 2.5 - Katello 4.1 (unsupported)",
+    path: "2.5",
+    builds: [
+      {
+        title: "Katello on EL",
+        filename: "index-foreman-deb.html",
+        guides: [
+          {
+            title: "Planning Guide",
+            path: "Planning_Guide"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Katello Server",
+            path: "Installing_Server_on_Red_Hat"
+          },
+          {
+            title: "Installing Smart Proxy with Content",
+            path: "Installing_Proxy_on_Red_Hat"
+          },
+          {
+            title: "Upgrading and Updating",
+            path: "Upgrading_and_Updating"
+          },
+          {
+            title: "Configuring Smart Proxies with a Load Balancer",
+            path: "Configuring_Load_Balancer"
+          },
+          {
+            title: "Content Management Guide",
+            path: "Content_Management_Guide"
+          },
+          {
+            title: "Converting a Host to RHEL",
+            path: "Converting_a_Host_to_RHEL"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Managing Hosts Guide",
+            path: "Managing_Hosts"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          },
+          {
+            title: "Application Centric Deployment",
+            path: "Application_Centric_Deployment"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: "Foreman 2.4 - Katello 4.0 (unsupported)",
+    path: "2.4",
+    builds: [
+      {
+        title: "Katello on EL",
+        filename: "index-foreman-deb.html",
+        guides: [
+          {
+            title: "Planning Guide",
+            path: "Planning_Guide"
+          },
+          {
+            title: "Quickstart Guide",
+            path: "Quickstart_Guide"
+          },
+          {
+            title: "Installing Katello Server",
+            path: "Installing_Server_on_Red_Hat"
+          },
+          {
+            title: "Installing Smart Proxy with Content",
+            path: "Installing_Proxy_on_Red_Hat"
+          },
+          {
+            title: "Upgrading and Updating",
+            path: "Upgrading_and_Updating"
+          },
+          {
+            title: "Configuring Smart Proxies with a Load Balancer",
+            path: "Configuring_Load_Balancer"
+          },
+          {
+            title: "Content Management Guide",
+            path: "Content_Management_Guide"
+          },
+          {
+            title: "Converting a Host to RHEL",
+            path: "Converting_a_Host_to_RHEL"
+          },
+          {
+            title: "Deploying Foreman on AWS",
+            path: "Deploying_on_AWS"
+          },
+          {
+            title: "Provisioning Guide",
+            path: "Provisioning_Guide"
+          },
+          {
+            title: "Configuring Foreman to use Ansible",
+            path: "Configuring_Ansible"
+          },
+          {
+            title: "Managing Hosts Guide",
+            path: "Managing_Hosts"
+          },
+          {
+            title: "Administering Foreman",
+            path: "Administering_Red_Hat_Satellite"
+          },
+          {
+            title: "Application Centric Deployment",
+            path: "Application_Centric_Deployment"
+          }
+        ]
+      }
+    ]
+  }
+]
