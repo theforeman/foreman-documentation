theforeman/foreman-documentation

commit ac63a51ce4d5e9d46112639a5e2f459ae2fdd43d

Migrate to nanoc and update CSS
Signed-off-by: Lukas Zapletal <lzap+git@redhat.com>

guides/common/Makefile
@@ -10,13 +10,15 @@ DEST_HTML = $(DEST_DIR)/index-$(BUILD).html
 DEST_PDF = $(BUILD_DIR)/$(subst doc-,,$(NAME))-$(BUILD).pdf
 IMAGES_DIR = $(DEST_DIR)/images
 IMAGES_TS = $(DEST_DIR)/.timestamp-images
-CSS_SOURCES = $(shell find $(COMMON_DIR)/sass -type f)
+CSS_SOURCES = $(shell find $(COMMON_DIR)/css -type f)
+DOCINFO_SOURCES = $(shell find $(COMMON_DIR)/ -type f -name docinfo\*.html)
 SOURCES = master.adoc $(shell find . -type f -name \*.adoc)
 DEPENDENCIES = $(shell find $(COMMON_DIR) -type f -name \*.adoc)
 IMAGES = $(shell find ./images ./common/images -type f 2>/dev/null || true)
 UNAME = $(shell uname)
 DATE_MDY = $(shell LC_ALL=C date "+%b %d %Y")
 DATE_MY = $(shell LC_ALL=C date "+%b %Y")
+CSS_STYLE = compressed
 
 ifeq ($(UNAME), Linux)
 BROWSER_OPEN = xdg-open
@@ -31,15 +33,17 @@ endif
 
 all: html
 
-html: prepare $(DEST_DIR)/$(BUILD).css $(IMAGES_TS) $(DEST_HTML)
-
-pdf: prepare $(DEST_PDF)
-
 prepare:
 	@mkdir -p $(BUILD_DIR) $(DEST_DIR) $(IMAGES_DIR) $(IMAGES_DIR) $(DEST_DIR)/common/images
 
+css: prepare $(DEST_DIR)/$(BUILD).css
+
+html: css $(IMAGES_TS) $(DEST_HTML)
+
+pdf: prepare $(DEST_PDF)
+
 clean:
-	@rm -rf "$(DEST_DIR)" "$(DEST_PDF)"
+	@rm -rf "$(DEST_DIR)" "$(DEST_PDF)" "$(BUILD_DIR)/default.css"
 
 browser: html
 	${BROWSER_OPEN} "file://$(realpath $(ROOTDIR)/$(DEST_HTML))"
@@ -54,7 +58,7 @@ open-pdf: pdf
 	${BROWSER_OPEN} "$(realpath $(ROOTDIR)/$(DEST_PDF))"
 
 $(BUILD_DIR)/default.css: $(CSS_SOURCES)
-	bundle exec compass compile --force -c $(COMMON_DIR)/sass_config.rb --sass-dir $(COMMON_DIR)/sass --css-dir $(BUILD_DIR)
+	bundle exec sass -C --style $(CSS_STYLE) -I $(COMMON_DIR)/css $(COMMON_DIR)/css/default.scss $@
 
 $(DEST_DIR)/$(BUILD).css: $(BUILD_DIR)/default.css
 	cp $< $@
@@ -64,8 +68,8 @@ $(IMAGES_TS): $(IMAGES)
 	pushd "$(IMAGES_DIR)" >/dev/null && for IMG in *.*; do ln -fs ../../images/$$IMG ../common/images/$$IMG; done && popd >/dev/null
 	@touch $(IMAGES_TS)
 
-$(DEST_HTML): $(SOURCES) $(DEPENDENCIES)
-	bundle exec asciidoctor -a build=$(BUILD) $(BASEURL_ATTR) -a docinfo=shared -a date_mdy="$(DATE_MDY)" -a date_my="$(DATE_MY)" -a docdatetime="$(DATE_MY)" -a stylesheet=$(BUILD).css -a linkcss=true -b html5 -d book --trace -o $@ $< 2>&1 | tee $@.log
+$(DEST_HTML): $(SOURCES) $(DEPENDENCIES) $(DOCINFO_SOURCES)
+	bundle exec asciidoctor -a build=$(BUILD) $(BASEURL_ATTR) -a docinfo=shared -a docinfodir=common -a date_mdy="$(DATE_MDY)" -a date_my="$(DATE_MY)" -a docdatetime="$(DATE_MY)" -a stylesheet=$(BUILD).css -a linkcss=true -b html5 -d book --trace -o $@ $< 2>&1 | tee $@.log
 	@echo CHECKING FOR ASCIIDOCTOR ERRORS AND WARNINGS
 	@! grep -Eq "^asciidoctor: (ERROR|WARNING)" $@.log
 	@echo CHECKING FOR ORPHAN XREFS HTML LINKS

web/Gemfile
@@ -1,0 +1,11 @@
+source "https://rubygems.org"
+
+gem "nanoc"
+gem "webrick"
+gem "asciidoctor"
+gem "sass"
+gem "w3c_validators"
+
+group 'nanoc' do
+  gem "nanoc-live"
+end

web/Makefile
@@ -1,10 +1,0 @@
-# Builds static site in public/ directory.
-# Add and push the changes into git.
-build:
-	hugo
-
-server:
-	hugo server
-
-server-all:
-	hugo server --bind 0.0.0.0

commit 2d0eb38562a7e06deff3c84196f0c4d13be51b58

don't depend on compass and zurb, we don't use it
but it wants a super old sass which doesn't work on modern Ruby

pull in sass directly instead

Fixes: ac63a51ce4d5e9d46112639a5e2f459ae2fdd43d

Dockerfile
@@ -12,7 +12,6 @@ RUN dnf upgrade -y && \
                    rubygem-asciidoctor-pdf && \
     dnf groupinstall -y development-tools
 
-RUN gem install compass:0.12.7 \
-                zurb-foundation:4.3.2
+RUN gem install sass
 
 WORKDIR /foreman-documentation/guides

Gemfile
@@ -5,5 +5,4 @@ gem 'asciidoctor-pdf'
 # missing dependency for asciidoctor-pdf
 gem 'matrix'
 
-gem 'compass', '0.12.7'
-gem 'zurb-foundation', '4.3.2'
+gem 'sass'

guides/common/Makefile
@@ -67,7 +67,7 @@ open-pdf: pdf
 	${BROWSER_OPEN} "$(realpath $(ROOTDIR)/$(DEST_PDF))"
 
 $(DEST_DIR)/$(BUILD).css: $(CSS_SOURCES)
-	bundle exec sass --cache-location $(DEST_DIR)/sass-cache --style $(CSS_STYLE) -I $(COMMON_DIR)/css $(COMMON_DIR)/css/default.scss $@
+	bundle exec sass --sourcemap=none --cache-location $(DEST_DIR)/sass-cache --style $(CSS_STYLE) -I $(COMMON_DIR)/css $(COMMON_DIR)/css/default.scss $@
 
 $(IMAGES_TS): $(IMAGES)
 	@[[ -h ./images/common ]] || echo "FAILURE: Missing ./images dir with ./images/common symlink to commons!"