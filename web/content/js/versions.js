theforeman/foreman-documentation

commit ddba2d718d91ade9bdfd3375f875cc883993449a

Branch Foreman 3.8

web/content/js/versions.js
@@ -184,6 +184,190 @@ const navVersions = [
     ]
   },
   {
+    "title": "Foreman 3.8 - Katello 4.10 (RC)",
+    "path": "3.8",
+    "builds": [
+      {
+        "title": "Foreman on EL",
+        "filename": "index-foreman-el.html",
+        "guides": [
+          {
+            "title": "Release Notes",
+            "path": "Release_Notes"
+          },
+          {
+            "title": "Quickstart Guide",
+            "path": "Quickstart"
+          },
+          {
+            "title": "Installing Foreman Server",
+            "path": "Installing_Server"
+          },
+          {
+            "title": "Installing Smart Proxy",
+            "path": "Installing_Proxy"
+          },
+          {
+            "title": "Deploying Foreman on AWS",
+            "path": "Deploying_Project_on_AWS"
+          },
+          {
+            "title": "Provisioning Hosts",
+            "path": "Provisioning_Hosts"
+          },
+          {
+            "title": "Managing Hosts",
+            "path": "Managing_Hosts"
+          },
+          {
+            "title": "Configuring Hosts Using Ansible",
+            "path": "Managing_Configurations_Ansible"
+          },
+          {
+            "title": "Configuring Hosts Using Puppet",
+            "path": "Managing_Configurations_Puppet"
+          },
+          {
+            "title": "Configuring Hosts Using Salt",
+            "path": "Managing_Configurations_Salt"
+          },
+          {
+            "title": "Managing Security Compliance",
+            "path": "Managing_Security_Compliance"
+          },
+          {
+            "title": "Administering Foreman",
+            "path": "Administering_Project"
+          },
+          {
+            "title": "Application Centric Deployment",
+            "path": "Deploying_Hosts_AppCentric"
+          }
+        ]
+      },
+      {
+        "title": "Foreman on Debian",
+        "filename": "index-foreman-deb.html",
+        "guides": [
+          {
+            "title": "Release Notes",
+            "path": "Release_Notes"
+          },
+          {
+            "title": "Quickstart Guide",
+            "path": "Quickstart"
+          },
+          {
+            "title": "Installing Foreman Server",
+            "path": "Installing_Server"
+          },
+          {
+            "title": "Deploying Foreman on AWS",
+            "path": "Deploying_Project_on_AWS"
+          },
+          {
+            "title": "Provisioning Hosts",
+            "path": "Provisioning_Hosts"
+          },
+          {
+            "title": "Configuring Hosts Using Ansible",
+            "path": "Managing_Configurations_Ansible"
+          },
+          {
+            "title": "Configuring Hosts Using Puppet",
+            "path": "Managing_Configurations_Puppet"
+          },
+          {
+            "title": "Administering Foreman",
+            "path": "Administering_Project"
+          }
+        ]
+      },
+      {
+        "title": "Katello on EL",
+        "filename": "index-katello.html",
+        "guides": [
+          {
+            "title": "Release Notes",
+            "path": "Release_Notes"
+          },
+          {
+            "title": "Planning Guide",
+            "path": "Planning_for_Project"
+          },
+          {
+            "title": "Quickstart Guide",
+            "path": "Quickstart"
+          },
+          {
+            "title": "Installing Katello Server",
+            "path": "Installing_Server"
+          },
+          {
+            "title": "Installing Smart Proxy with Content",
+            "path": "Installing_Proxy"
+          },
+          {
+            "title": "Upgrading Katello",
+            "path": "Upgrading_Project"
+          },
+          {
+            "title": "Tuning Performance",
+            "path": "Tuning_Performance"
+          },
+          {
+            "title": "Configuring Smart Proxies with a Load Balancer",
+            "path": "Configuring_Load_Balancer"
+          },
+          {
+            "title": "Managing Organizations and Locations in Foreman",
+            "path": "Managing_Organizations_and_Locations"
+          },
+          {
+            "title": "Managing Content",
+            "path": "Managing_Content"
+          },
+          {
+            "title": "Deploying Foreman on AWS",
+            "path": "Deploying_Project_on_AWS"
+          },
+          {
+            "title": "Provisioning Hosts",
+            "path": "Provisioning_Hosts"
+          },
+          {
+            "title": "Managing Hosts",
+            "path": "Managing_Hosts"
+          },
+          {
+            "title": "Configuring Hosts Using Ansible",
+            "path": "Managing_Configurations_Ansible"
+          },
+          {
+            "title": "Configuring Hosts Using Puppet",
+            "path": "Managing_Configurations_Puppet"
+          },
+          {
+            "title": "Configuring Hosts Using Salt",
+            "path": "Managing_Configurations_Salt"
+          },
+          {
+            "title": "Managing Security Compliance",
+            "path": "Managing_Security_Compliance"
+          },
+          {
+            "title": "Administering Foreman",
+            "path": "Administering_Project"
+          },
+          {
+            "title": "Application Centric Deployment",
+            "path": "Deploying_Hosts_AppCentric"
+          }
+        ]
+      }
+    ]
+  },
+  {
     "title": "Foreman 3.7 - Katello 4.9 (supported)",
     "path": "3.7",
     "builds": [

Makefile
@@ -1,7 +1,7 @@
 SHELL := /bin/bash
 DEST := result
 PORT := 5000
-VERSION_LINKS := 3.7 3.6 3.5 3.4 3.3 3.2 3.1 3.0 2.5 2.4
+VERSION_LINKS := 3.8 3.7 3.6 3.5 3.4 3.3 3.2 3.1 3.0 2.5 2.4
 
 .PHONY: all clean html web compile serve prep
 

commit 493d1638f46c3e5b003d09c8c5bb25339e53867c

Correct Foreman 3.8 navigation
Various guides are hidden because they're not ready. These should not
show up in the navigation.

Fixes: ddba2d718d91ade9bdfd3375f875cc883993449a ("Branch Foreman 3.8")

web/content/js/versions.js
@@ -196,52 +196,8 @@ const navVersions = [
             "path": "Release_Notes"
           },
           {
-            "title": "Quickstart Guide",
-            "path": "Quickstart"
-          },
-          {
-            "title": "Installing Foreman Server",
-            "path": "Installing_Server"
-          },
-          {
-            "title": "Installing Smart Proxy",
-            "path": "Installing_Proxy"
-          },
-          {
-            "title": "Deploying Foreman on AWS",
-            "path": "Deploying_Project_on_AWS"
-          },
-          {
-            "title": "Provisioning Hosts",
-            "path": "Provisioning_Hosts"
-          },
-          {
-            "title": "Managing Hosts",
-            "path": "Managing_Hosts"
-          },
-          {
             "title": "Configuring Hosts Using Ansible",
             "path": "Managing_Configurations_Ansible"
-          },
-          {
-            "title": "Configuring Hosts Using Puppet",
-            "path": "Managing_Configurations_Puppet"
-          },
-          {
-            "title": "Configuring Hosts Using Salt",
-            "path": "Managing_Configurations_Salt"
-          },
-          {
-            "title": "Managing Security Compliance",
-            "path": "Managing_Security_Compliance"
-          },
-          {
-            "title": "Administering Foreman",
-            "path": "Administering_Project"
-          },
-          {
-            "title": "Application Centric Deployment",
-            "path": "Deploying_Hosts_AppCentric"
           }
         ]
       },
@@ -254,32 +210,8 @@ const navVersions = [
             "path": "Release_Notes"
           },
           {
-            "title": "Quickstart Guide",
-            "path": "Quickstart"
-          },
-          {
-            "title": "Installing Foreman Server",
-            "path": "Installing_Server"
-          },
-          {
-            "title": "Deploying Foreman on AWS",
-            "path": "Deploying_Project_on_AWS"
-          },
-          {
-            "title": "Provisioning Hosts",
-            "path": "Provisioning_Hosts"
-          },
-          {
             "title": "Configuring Hosts Using Ansible",
             "path": "Managing_Configurations_Ansible"
-          },
-          {
-            "title": "Configuring Hosts Using Puppet",
-            "path": "Managing_Configurations_Puppet"
-          },
-          {
-            "title": "Administering Foreman",
-            "path": "Administering_Project"
           }
         ]
       },