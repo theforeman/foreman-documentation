e3ffa5d59def52598397321f1a1d64188ef290b8
Maximilian Kolb <kolb@atix.de>
2023-10-09 14:47:05 +0200

    Consolidate version attribute
    $ find guides -type f -iname "*.adoc" | xargs sed -i "s/ProductVersionPrevious/ProjectVersionPrevious/g"
    $ find guides -type f -iname "*.adoc" | xargs sed -i "s/ProductVersion/ProjectVersion/g"

--- guides/scripts/upstreamize.rb
+++ guides/scripts/upstreamize.rb
@@ -27,7 +27,7 @@ report.write("# Upstreamization report from #{Time.now.utc}\n")
             next
           end
           line.gsub!(%r{https?://access.redhat.com/documentation/en-us/red_hat_satellite/\d\.\d/html/}, '{BaseURL}')
-          line.gsub!(%r{https?://access.redhat.com/documentation/en-us/red_hat_satellite/{ProductVersion}/html/}, '{BaseURL}')
+          line.gsub!(%r{https?://access.redhat.com/documentation/en-us/red_hat_satellite/{ProjectVersion}/html/}, '{BaseURL}')
           line.gsub!(/Red( |\u{00A0}|{nbsp}) Hat Satellite( |\u{00A0}|{nbsp})6.\d/, '{ProjectNameX}')
           line.gsub!(/Red( |\u{00A0}|{nbsp})Hat Satellite( |\u{00A0}|{nbsp})6/, '{ProjectNameX}')
           line.gsub!(/Red( |\u{00A0}|{nbsp})Hat Satellite/, '{ProjectName}')
