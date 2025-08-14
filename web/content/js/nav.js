6d9560f49efa750ff46ca6fbb227327fa0f47fc9
Aneta Šteflová Petrová <apetrova@redhat.com>
2025-08-14 09:41:43 +0200

    Fix logo heading (#4156)
    * Drop text from logo and revert back to helmet only

--- web/content/js/nav.js
+++ web/content/js/nav.js
@@ -16,9 +16,8 @@ function buildNavigation() {
   }
 
   return `<nav>
-  <a class="nav-logo-heading" href="/">
+  <a href="/">
     <img class="logo" src="/img/helmet.svg" alt="Home">
-    <span class="nav-heading">Foreman docs</span>
   </a>
 <button type="button" class="btn-hamburger" data-action="nav-toggle">
   <span></span>
